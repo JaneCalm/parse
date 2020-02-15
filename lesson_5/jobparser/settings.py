160b98720e414f1d639d23fd53b1ccf3e14e0728
JaneCalm <drugsdream@gmail.com>
2020-02-15 21:48:45 +0300

    /

--- lesson_5/jobparser/settings.py
+++ lesson_5/jobparser/settings.py
@@ -16,10 +16,14 @@ NEWSPIDER_MODULE = 'jobparser.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'jobparser (+http://www.yourdomain.com)'
+USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
+
+LOG_ENABLED = True
+LOG_LEVEL = 'DEBUG' #INFO ERROR
+# LOG_FILE = 'log.txt'
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
@@ -64,9 +68,9 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'jobparser.pipelines.JobparserPipeline': 300,
-#}
+ITEM_PIPELINES = {
+    'jobparser.pipelines.JobparserPipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
--- lesson_5/jobparser/spiders/hhru.py
+++ lesson_5/jobparser/spiders/hhru.py
@@ -1,11 +1,28 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from scrapy.http import HtmlResponse
+from jobparser.items import JobparserItem
+import re
 
 
 class HhruSpider(scrapy.Spider):
     name = 'hhru'
     allowed_domains = ['hh.ru']
-    start_urls = ['http://hh.ru/']
+    search_s = input('Введите название вакансии: ')
+    search = re.sub(r'\s', '+', search_s)
+    start_urls = [f'https://hh.ru/search/vacancy?area=1&clusters=true&enable_snippets=true&order_by=publication_time&text={search}&salary=']
 
-    def parse(self, response):
-        pass
+    def parse(self, response: HtmlResponse):
+        next_page = response.xpath("//a[@class='bloko-button HH-Pager-Controls-Next HH-Pager-Control']/@href").extract_first()
+        vacansy = response.xpath(
+            "//div[@class='resume-search-item__name']//a/@href"
+        ).extract()
+        for link in vacansy:
+            yield response.follow(link, callback=self.vacansy_parse)
+        yield response.follow(next_page, callback=self.parse)
+
+    def vacansy_parse(self, response: HtmlResponse):
+        name = response.xpath('//h1/text()').extract_first()
+        salary = response.xpath("//p[@class='vacancy-salary']/text()").extract()
+        link = response.url
+        yield JobparserItem(_id=link, name=name, salary=salary, link=link, from_url=self.allowed_domains[0])
