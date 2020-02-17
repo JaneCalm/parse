642e7a4414fda6a3c8efeadea6ddbab7c3626872
JaneCalm <drugsdream@gmail.com>
2020-02-17 19:20:39 +0300

    Причесала поисковик вакансий

--- lesson_5/jobparser/items.py
+++ lesson_5/jobparser/items.py
@@ -6,13 +6,19 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from scrapy.loader.processors import MapCompose, TakeFirst
 
 
 class JobparserItem(scrapy.Item):
     # define the fields for your item here like:
-    _id = scrapy.Field()
-    name = scrapy.Field()
+    _id = scrapy.Field(output_processor=TakeFirst())
+    name = scrapy.Field(output_processor=TakeFirst())
     salary = scrapy.Field()
-    link = scrapy.Field()
-    from_url = scrapy.Field()
+    link = scrapy.Field(output_processor=TakeFirst())
+    from_url = scrapy.Field(output_processor=TakeFirst())
+
+    def __setitem__(self, key, value):
+        if key not in self.fields:
+            self.fields[key] = scrapy.Field()
+        super(JobparserItem, self).__setitem__(key, value)
     pass
